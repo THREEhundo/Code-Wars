793bce4d29357b095307bcadc945918a9386eb95
THREEhundo <samuelbaik87@gmail.com>
2022-05-19 12:20:48 -0700

    daily kata

---
+++ js/sort-the-odd.js
@@ -0,0 +1,19 @@
+/****************************/
+/*** Sort the Odd			5/19
+/***************************/
+
+/***
+ * You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+ ***/
+
+/**
+ *
+ */
+
+const sortArray = (array) => {
+	// filter out all odd numbers
+	const odd = array.filter((x) => x % 2).sort((a, b) => a - b)
+	// odd.shift() removes first item of array and returns it!
+	return array.map((x) => (x % 2 ? odd.shift() : x))
+}
+console.log(sortArray([5, 8, 6, 3, 4]))
