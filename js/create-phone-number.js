63b60ac5c12d68f3c309d3811c188678ac092b69
THREEhundo <samuelbaik87@gmail.com>
2022-06-10 18:36:32 -0700

    best practice added

--- js/create-phone-number.js
+++ js/create-phone-number.js
@@ -29,3 +29,12 @@ const createPhoneNumber = (arr) => {
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+const createPhoneNumberBestPractice = (arr) => {
+	let format = '(xxx) xxx-xxxx'
+
+	for (let i = 0; i < numbers.length; i++) {
+		format = format.replace('x', numbers[i])
+	}
+	return format
+}
