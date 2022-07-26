03d80d776b5d2cf8e1b100b60b0da7045bbaed6a
THREEhundo <samuelbaik87@gmail.com>
2022-07-25 17:02:22 -0700

    daily kata

---
+++ js/duplicate-encoder.js
@@ -0,0 +1,39 @@
+/****************************/
+/*** Duplicate Encoder 7/25
+/***************************/
+
+/**
+ * The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string.
+ *
+ * Ignore capitalization when determining if a character is a duplicate.
+ *
+ */
+
+const duplicateEncode = (word) => {
+	// each character is === (
+
+	// repeat characters === )
+
+	/**
+	 * break up string
+	 * turn into an array
+	 * map and convert all string to lowercase
+	 * for each letter check against other letters in the array for an exact match
+	 * use some() on array in map()
+	 * if true (
+	 * if false )
+	 * join the new array
+	 * return new string
+	 *
+	 */
+	let copy = word.toLowerCase().split('')
+	return word
+		.toLowerCase()
+		.split('')
+		.map((letter, i, word) => {
+			return copy.some((item, j) => letter === item && i !== j) ? ')' : '('
+		})
+		.join('')
+}
+
+console.log(duplicateEncode('Sammy'))
