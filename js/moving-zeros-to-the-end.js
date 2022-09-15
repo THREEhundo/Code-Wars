bb1e66d0d060a4469b3ed0a58d0f219ae9021173
THREEhundo <samuelbaik87@gmail.com>
2022-09-15 14:24:15 -0700

    daily kata

---
+++ js/moving-zeros-to-the-end.js
@@ -0,0 +1,19 @@
+//* Moving Zeros to the End *//
+/**
+ * Write an algorithm that takes an array and moves all of the zeroes to the end, preserving the order of the other elements.
+ */
+
+const moveZeros = arr => {
+	// find index of zero's
+	// store in variable
+	//remove the zeros
+	// push to end of new array
+	let zArr = []
+	let reorder = []
+	arr.map(x =>
+		x !== 0 ? (reorder = [...reorder, x]) : (zArr = [...zArr, x])
+	)
+	return [...reorder, ...zArr]
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a']))
