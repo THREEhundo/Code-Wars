6e90630eb5133d245f9ef6be9122c760a2f981a2
THREEhundo <samuelbaik87@gmail.com>
2022-10-13 15:54:34 -0700

    daily kata

--- js/extract-domain-name-from-URL.js
+++ js/extract-domain-name-from-URL.js
@@ -12,29 +12,12 @@
 // use includes method if 'www.' is included
 // log where '//' ends
 
-const domainName = (url) => {
-	let str = ''
-	if (url.includes('www.')) {
-		let cut = url.split('.')
-		return (str = cut[1])
-	}
-	if (url.includes('://') && !url.includes('www.')) {
-		let u = url.split('://')
-		str = u[1]
-	}
-	if (str == '') str = url
-	if (str.includes('.')) {
-		let n = str.split('.')
-		str = n[0]
-	}
-	return str
-}
-
-const domainNameReplace = (url) => {
-	url.replace('https://', '')
-	url.replace('http://')
-	url.replace('www.', '')
-	return url.split('.')[0]
-}
-
+const domainName = url =>
+	url
+		.replace('http://', '')
+		.replace('http://www.', '')
+		.replace('https://www.', '')
+		.replace('www.', '')
+		.replace('https://', '')
+		.split('.')[0]
 console.log(domainName('i3higfb50brpne6ef-t.tv/index.php'))
