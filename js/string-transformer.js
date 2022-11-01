5e030233f14995a21b91a774bd274a35a4652c9e
THREEhundo <samuelbaik87@gmail.com>
2022-11-01 15:14:34 -0700

    daily kata

---
+++ js/string-transformer.js
@@ -0,0 +1,28 @@
+/** String Transformer
+ *
+ * Given a string, return a new string that has transformed based on the input:
+ *
+ * Change case of every character, ie. lower case to upper case, upper case to lower case.
+ *
+ * Reverse the order of words from the input.
+ *
+ * Note: You will have to handle multiple spaces, and leading/trailing spaces.
+ */
+
+const stringTransformer = str =>
+	str
+		.trim()
+		.split(' ')
+		.map(word =>
+			word
+				.split('')
+				.map(ltr =>
+					ltr.toUpperCase() == ltr
+						? ltr.toLowerCase()
+						: ltr.toUpperCase()
+				)
+				.join('')
+		)
+		.join(' ')
+
+console.log(stringTransformer('Example Input'))
