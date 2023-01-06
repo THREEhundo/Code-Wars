4e186fb15f9b93da0dc6bea1d032758d3fa69d34
THREEhundo <samuelbaik87@gmail.com>
2023-01-05 17:25:17 -0800

    daily kata

---
+++ js/messi-goals.js
@@ -0,0 +1,11 @@
+/*** Grasshopper - Messi Goal Function
+ * Messi is a soccer player with goals in three leagues:
+ *
+ * LaLiga
+ * Copa del Rey
+ * Champions
+ *
+ * Complete the function to return his total number of goals in all three leagues.
+ */
+
+const goals = (g1, g2, g3) => g1 + g2 + g3
