90e0d75eb7827027a893d3feeba1396976915d68
THREEhundo <samuelbaik87@gmail.com>
2023-01-13 23:40:59 -0800

    daily kata

---
+++ js/squaring-an-argument.js
@@ -0,0 +1,8 @@
+/***
+ * Function 2 - Squaring an argument
+ * 
+ * Now you have to write a function that takes an argument and returns the square of it.
+ * 
+ */
+
+const square = n => Math.pow(n, 2)
