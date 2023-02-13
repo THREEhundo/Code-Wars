2d01c0eb2c66b1d72b13ff9387669fa7bb543c06
THREEhundo <samuelbaik87@gmail.com>
2023-02-12 19:57:06 -0800

    daily kata

---
+++ js/convert-boolean-to-string.js
@@ -0,0 +1,6 @@
+/*** Convert Boolean to String
+ *
+ * Implement a function which convert the given boolean value into its string representation.
+ */
+
+const booleanToString = t => (t == true ? 'true' : 'false')
