2998fd0193c0a841cb1b1ebe5fa27af50ccc87a3
THREEhundo <samuelbaik87@gmail.com>
2023-02-25 14:19:21 -0800

    daily kata

---
+++ js/pair-of-gloves.js
@@ -0,0 +1,34 @@
+/*** Pair of Gloves
+ *
+ * Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+ *
+ * Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the
+ * same color can form pairs.
+ *
+ * Examples:
+ * input = ["red", "green", "red", "blue", "blue"]
+ * result = 2 (1 red pair + 1 blue pair)
+ * input = ["red", "red", "red", "red", "red", "red"]
+ * result = 3 (3 red pairs)
+ *
+ */
+
+const numberOfPairs = gloves => {
+	let pairs = 0
+	let count = gloves.reduce((allColors, currentColor) => {
+		const currCount = allColors[currentColor] ?? 0
+		return {
+			...allColors,
+			[currentColor]: currCount + 1
+		}
+	}, {})
+
+	for (const color in count) {
+		pairs += Math.floor(count[color] / 2)
+	}
+	return pairs
+}
+
+console.log(
+	numberOfPairs(['red', 'red', 'red', 'red', 'red', 'red', 'blue', 'blue'])
+)
