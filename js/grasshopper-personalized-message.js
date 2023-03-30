d9395b868ab350634338188cd8016991c9e86e33
THREEhundo <samuelbaik87@gmail.com>
2023-03-29 18:36:07 -0700

    daily kata

---
+++ js/grasshopper-personalized-message.js
@@ -0,0 +1,6 @@
+/*** Grasshopper - Personalized Message
+ *
+ * Create a function that gives a personalized message greeting. This function takes two parameters: name & owner.
+ */
+
+const greet = (n, o) => (n === o ? 'Hello boss' : 'Hello guest')
